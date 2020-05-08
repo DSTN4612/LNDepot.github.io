2b5fe71f57cc3f2f697905b6d332d7257404bba3
DSTN4612 <DroneSurveyTN@gmail.com>
2020-05-08 07:05:08 -0400

    Initial commit

---
+++ pointclouds/LNDepot/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 8335042,
+    "boundingBox": {
+        "lx": 724933.33,
+        "ly": 3911659.25,
+        "lz": 145.07,
+        "ux": 725092.07,
+        "uy": 3911817.99,
+        "uz": 303.8099999999907
+    },
+    "tightBoundingBox": {
+        "lx": 724933.33,
+        "ly": 3911659.25,
+        "lz": 145.07,
+        "ux": 725092.0700000001,
+        "uy": 3911809.71,
+        "uz": 165.1
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 1.3747286796569825,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
